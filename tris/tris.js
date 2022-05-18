0dcea20ac641514b387762d54bdeefe5f726908d
Irina Dochia <iryydo@gmail.com>
2022-05-18 13:10:52 +0200

    creato array vincite

--- tris/tris.js
+++ tris/tris.js
@@ -1,5 +1,22 @@
 const celle = document.querySelectorAll(".cella");
 let turnoX = true;
+const combinazioniVincenti = [
+
+    //ORIZZONTALI
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //DIAGONALI
+    [0, 4, 8],
+    [2, 4, 6]
+    //VERTICALI
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+]
+
+
 celle.forEach(cella => {
     cella.addEventListener("click", onClick, { once: true });
 })
@@ -15,6 +32,8 @@ function onClick(evento) {
         cellaSingola.classList.add("cerchio");
         turnoX = true;
     }
+
+
     //aggiungere icona
     //stabilire vincitore
     //controllare patta
